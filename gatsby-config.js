6aa48dfd0c902367b6fb2b54820e76cc2e212ea1
Zen <zengatsuu@gmail.com>
2021-02-25 01:38:22 +0100

    Fix styled-components not pre-rendering

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+  ],
 }
